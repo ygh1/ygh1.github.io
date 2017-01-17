ebf9d57494c93fa08d4f47e356dcef45b45303e1
yangh <13521420643@163.com>
2017-01-17 22:30:53 +0800

    clean

--- row4/JD/js/orderJs.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by luoluo on 2016/10/26.
- */
-var order = document.getElementById('order');
-var clientH = document.documentElement.clientHeight || document.body.clientHeight;
-order.style.height = clientH + 'px';
